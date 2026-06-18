83ff5a61cfa3fcec3a3682b41a27f996c8ddad9d
egmontes <eddygmontes@gmail.com>
2026-06-18 00:29:37 -0400

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAShw_6Iv6GJxmo29YxLkwX0rv3YuXSQ5U",
-  authDomain: "wedding-planning-289a8.firebaseapp.com",
-  projectId: "wedding-planning-289a8",
-  storageBucket: "wedding-planning-289a8.firebasestorage.app",
-  messagingSenderId: "313362929175",
-  appId: "1:313362929175:web:0ee378274d79603cfcee52",
-  measurementId: "G-W9NWS9G0V7"
-};
